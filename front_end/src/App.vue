b7e40428995f534427db90a1c1aaf3a62117a648
grayson.b.gordon <twiggy.gordon1@gmail.com>
2021-07-26 13:20:16 -0400

    Adds a todos component using a for loop

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Todos />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld,
+    Todos
   }
 }
 </script>
---
+++ front_end/src/components/Todos.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+      <h1>TODOS BOX</h1>
+      <ul>
+        <li v-for="item in items"  v-bind:key="item.id">
+            {{ item.message }}
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todos',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      items: [{ id: 1, message: 'Foo' }, { id: 2, message: 'Bar' }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
